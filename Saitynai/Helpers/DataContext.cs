0d8256bb89bfecb02ca06c758eb3b16b116bd6b1
modestas <modestaskris@gmail.com>
2022-09-28 21:56:13 +0300

    Added dataContext class

--- Saitynai/Helpers/DataContext.cs
+++ Saitynai/Helpers/DataContext.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System.Security.Cryptography;
+using Microsoft.EntityFrameworkCore;
+using Saitynai.Classes;
 using Saitynai.Models;
 
 namespace Saitynai.Helpers;
@@ -8,6 +10,7 @@ public class DataContext : DbContext
     public DbSet<User> Users { get; set; }
     public DbSet<Playlist> Playlists { get; set; }
     public DbSet<Song> Songs { get; set; }
+    public DbSet<Category>? Category { get; set; }
 
     public string DbPath { get; }
 
@@ -24,5 +27,35 @@ public class DataContext : DbContext
         
         => options.UseSqlServer(_configuration.GetConnectionString("YPSApiDatabase"));
 
-    // TODO add connection string
+    // The following configures EF to create a Sqlite database file in the
+    // special "local" folder for your platform.
+
+
+    #region Required
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        string password = "admin";
+        CreatePasswordHash(password, out byte[] passwordHash, out byte[] passwordSalt);
+
+        modelBuilder.Entity<User>().HasData(new User
+        {
+            UserId = -1,
+            Username= "admin",
+            PasswordHash = passwordHash,
+            PasswordSalt = passwordSalt,
+            Role = Role.Admin,
+            Categories = new List<Category>(),
+        });
+
+    }
+    #endregion
+
+    private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+    {
+        using (var hmac = new HMACSHA512())
+        {
+            passwordSalt = hmac.Key;
+            passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+        }
+    }
 }
