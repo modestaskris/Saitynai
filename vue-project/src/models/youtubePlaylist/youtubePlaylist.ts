388de839723cc4e67f73ca5da3dcddf14bf5a51b
modestas <modestaskris@gmail.com>
2022-10-03 17:11:21 +0300

    Deleted unused components, interfaces

--- vue-project/src/models/youtubePlaylist/youtubePlaylist.ts
+++
@@ -1,5 +0,0 @@
-import type { IYoutubePlaylistCreate } from "./interfaces";
-
-export const youtubePlaylistCreate: IYoutubePlaylistCreate = {
-    url: ''
-}
--- vue-project/src/models/youtubeSong/interfaces.ts
+++
@@ -1,6 +0,0 @@
-export interface IYoutubeSong{
-    id: number,
-    url: string
-    title:string
-    downladed: boolean;
-}
