f525808578b40ea6083c4aaf10337fcfbd017611
Modestas <modestaskris@gmail.com>
2022-10-06 18:39:49 +0300

    Created navbar

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,28 +1,10 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from "vue-router";
+import Navbar from "./components/Navbar.vue";
 </script>
 
 <template>
-  <header>
-    <nav>
-      <!-- <RouterLink to="/">Home</RouterLink> -->
-      <!-- <br/> -->
-      <!-- <RouterLink to="/about">About</RouterLink> -->
-      <!-- TODO ROUTE CONSTANT DOES NOT WORK HERE  -->
-      <!-- Maybe router ling object shit -->
-      <!-- <RouterLink to={{ROUTE.REGISTER}}>Register</RouterLink>  -->
-      <br/>
-      <hr/>
-      <RouterLink to='/register'>Register</RouterLink> 
-      <br/>
-      <RouterLink to="/login">login</RouterLink> 
-      <br/>
-      <RouterLink to="/categories">categories</RouterLink> 
-      <br/>
-      <hr/>
-    </nav>
-    
-  </header>
+  <Navbar/>
 
   <RouterView />
 </template>
---
+++ vue-project/src/components/Navbar.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { defineComponent } from 'vue';
+</script>
+
+<template>
+    <div class="justify-center flex mb-4">
+        <div class="flex justify-center bg-stone-200 p-2 rounded-b-full w-3/4">
+            <div>
+                <RouterLink to='/register' class="mx-2">Register</RouterLink>
+            </div>
+            <div>
+                <RouterLink to="/login">Login</RouterLink>
+            </div>
+            <div>
+                <RouterLink to="/categories">Categories</RouterLink>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+export default defineComponent({
+    data() {
+        return {
+        }
+    }
+
+})
+</script>
