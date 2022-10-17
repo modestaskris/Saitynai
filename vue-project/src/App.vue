e4c637110fde462267da1dc9b943e771d1697e25
modestaskris <modestaskris@gmail.com>
2022-10-17 17:54:09 +0300

    Added google font

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -5,7 +5,10 @@ import LayoutView from "./views/LayoutView.vue";
 </script>
 
 <template>
+  <!-- <div class="fafa"> -->
   <LayoutView />
+  <!-- </div> -->
+  <!-- <LayoutView /> -->
 </template>
 
 <script lang="ts">
@@ -15,3 +18,20 @@ export default defineComponent({
   }
 })
 </script>
+
+
+<style>
+html,
+body {
+  font-family: 'Varela Round', sans-serif;
+}
+
+.fafa {
+  background: #F7F8F8;
+  /* fallback for old browsers */
+  background: -webkit-linear-gradient(to right, #ACBB78, #F7F8F8);
+  /* Chrome 10-25, Safari 5.1-6 */
+  background: linear-gradient(to right, #ACBB78, #F7F8F8);
+  /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+}
+</style>
